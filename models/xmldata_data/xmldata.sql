9d73b3dc48d86cbc53054ec12ada75fc077da19c
JITENDRA CHATURVEDI <JITENDRA.CHATURVEDI@ibm.com>
2025-04-21 11:07:33 +0000

    commit

--- models/xmldata_data/xmldata.sql
+++ models/xmldata_data/xmldata.sql
@@ -5,9 +5,4 @@ XMLGET(s.value, 'Email'):"$"::string as "Email",
 XMLGET(s.value, 'Age'):"$"::int as "Age",
 XMLGET(s.value, 'City'):"$"::string as "City"
 FROM MYDB.AZUREXML.XMLTABLE,
-LATERAL FLATTEN(TO_ARRAY(MYDB.AZUREXML.XMLTABLE._DATA:"$")) s
- 
- 
- 
- 
- 
+LATERAL FLATTEN(TO_ARRAY(MYDB.AZUREXML.XMLTABLE._DATA:"$")) s 

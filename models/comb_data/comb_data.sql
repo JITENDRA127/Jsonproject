f2b75b19377ea517baf3e0b2e07f9bb6b92fb1ac
JITENDRA CHATURVEDI <JITENDRA.CHATURVEDI@ibm.com>
2025-04-22 13:35:55 +0000

    commit

--- models/comb_data/comb_data.sql
+++ models/comb_data/comb_data.sql
@@ -1,3 +1,3 @@
-select  a.customer_id,a.first_name,a.last_name,a.accountnumber,a.balance,x."Transaction_Date"
+select  a.customer_id,a.first_name,a.last_name,a.accountnumber,a.balance,x."Transaction_Date",x."Transaction_Amount"
  from  MYDB.DBT_CASESTUDY.JSON1 a ,MYDB.DBT_CASESTUDY.XMLSQL x
  where a.customer_id = x."Customer_ID"
